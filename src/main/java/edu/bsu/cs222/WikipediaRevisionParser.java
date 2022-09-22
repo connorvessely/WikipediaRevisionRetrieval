60bef4f97fbbf2139bf4962f60433d4ea3fd71aa
GioGote <sivelasco.cs@gmail.com>
2022-09-22 13:16:11 -0400

    Gio Velasco <sivelasco@bsu.edu> build wikipediaRevisionParser still trying to figure out JsonPath

--- src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
@@ -1,9 +1,13 @@
 package edu.bsu.cs222;
 
+import com.jayway.jsonpath.JsonPath;
+
+import java.io.IOException;
 import java.io.InputStream;
 
 public class WikipediaRevisionParser {
-    public String parse(InputStream testDataStream){
-
+    public String parse(InputStream testDataStream) throws IOException {
+        Object o = JsonPath.read(testDataStream, "$..timestamp");
+        return null;
     }
 }
